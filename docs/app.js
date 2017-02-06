2d4f9dec2124c396a7f925e493185ddd96395d52
Yun <Yunseong_Seo@worldvision.ca>
2017-02-06 11:28:46 -0500

    Fix bug

--- docs/app.js
+++ docs/app.js
@@ -7,13 +7,18 @@
 	LunchCheckController.$inject = ['$scope', '$filter'];
 	function LunchCheckController($scope, $filter) {
 		$scope.checkTooMuch = function() {
-			var foods = $scope.input_foods.split(',');
-			foods = foods.filter(function(e) { return e.trim().length !== 0 });
-
-			if (foods.length > 3) {
-			 	$scope.message_result = "Too much!"
+			console.log($scope.input_foods);
+			if ($scope.input_foods == undefined || $scope.input_foods.trim().length == 0) {
+				$scope.message_result = "Please enter data first";
 			} else {
-				$scope.message_result = "Enjoy!"
+				var foods = $scope.input_foods.split(',');
+				foods = foods.filter(function(e) { return e.trim().length !== 0 });
+
+				if (foods.length > 3) {
+				 	$scope.message_result = "Too much!"
+				} else {
+					$scope.message_result = "Enjoy!"
+				}
 			}
 		}
 	}
